08d4ce873091d44b5162ae7c566633998825bfbe
harshtech123 <chourasiya400t@gmail.com>
2025-07-25 15:30:30 +0530

    removed unused commented code

--- graph/arangodb/src/connection.rs
+++ graph/arangodb/src/connection.rs
@@ -14,119 +14,12 @@ impl ProviderGraph for Graph {
 
 impl GuestGraph for Graph {
     fn begin_transaction(&self) -> Result<TransactionResource, GraphError> {
-        // Ensure common collections exist before starting transaction
-        // This is act as just helper for testing purposes
-        // let common_collections = vec![
-        //     (
-        //         "Person",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "TempUser",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Company",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Employee",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Node",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Product",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "User",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "KNOWS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "WORKS_FOR",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "CONNECTS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "FOLLOWS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        // ];
-
-        // // Create collections if they don't exist
-        // for (name, container_type) in common_collections {
-        //     let _ = self.api.ensure_collection_exists(name, container_type);
-        // }
-
         let transaction_id = self.api.begin_dynamic_transaction(false)?;
         let transaction = Transaction::new(self.api.clone(), transaction_id);
         Ok(TransactionResource::new(transaction))
     }
 
     fn begin_read_transaction(&self) -> Result<TransactionResource, GraphError> {
-        // Ensure common collections exist before starting transaction
-        // This is act as just helper for testing purposes
-        // let common_collections = vec![
-        //     (
-        //         "Person",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "TempUser",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Company",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Employee",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Node",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "Product",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "User",
-        //         golem_graph::golem::graph::schema::ContainerType::VertexContainer,
-        //     ),
-        //     (
-        //         "KNOWS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "WORKS_FOR",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "CONNECTS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        //     (
-        //         "FOLLOWS",
-        //         golem_graph::golem::graph::schema::ContainerType::EdgeContainer,
-        //     ),
-        // ];
-
-        // // Create collections if they don't exist
-        // for (name, container_type) in common_collections {
-        //     let _ = self.api.ensure_collection_exists(name, container_type);
-        // }
 
         let transaction_id = self.api.begin_dynamic_transaction(true)?;
         let transaction = Transaction::new(self.api.clone(), transaction_id);
